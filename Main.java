4fc023f762a5ac4214f935f60b869ea4a7b4bb4e
Lizoch364 <VasilevaE.04@mail.ru>
2024-11-22 14:58:50 +0300

    3 commit

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public static void main(String[]args){
         public class Main {
             System.out.println("Это добавлено в ветке main");
             System.out.println("Первый коммит");
-            System.out.println("Второй коммит")
+            System.out.println("Второй коммит");
+            System.out.println("Третий коммит");
     }
 }
