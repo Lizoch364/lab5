d13a8de993eb23cc25d744e9693db9033f3dfb28
Lizoch364 <VasilevaE.04@mail.ru>
2024-11-22 17:06:54 +0300

    3 commit dev

--- Veb.java
+++ Veb.java
@@ -2,6 +2,7 @@ public static void main(String[]args){
         public class Veb{
             System.out.println("1 коммит для веба");
             System.out.println("2 коммит для веба");
+            System.out.println("3 коммит для веба");
 
 
         }
